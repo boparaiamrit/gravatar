1306a664a8d466df88a89110df73dcedcc641e2d
Amritpal Singh <boparaiamrit@gmail.com>
2016-09-10 19:04:59 +0530

    Deferred True

--- src/GravatarServiceProvider.php
+++ src/GravatarServiceProvider.php
@@ -11,7 +11,7 @@ class GravatarServiceProvider extends ServiceProvider
 	 *
 	 * @var bool
 	 */
-	protected $defer = false;
+	protected $defer = true;
 	
 	/**
 	 * Bootstrap the application events.
